d3eaf52c6dc8b1887646cb6748c88d0962e5e9f7
jaya khandelwal <jayakhandelwal750@gmail.com>
2026-03-16 09:30:04 +0530

    first commit

---
+++ Calender/index.js
@@ -0,0 +1,33 @@
+let input = document.getElementById("input");
+let buttons = document.querySelectorAll("button");
+
+let string = "";
+
+buttons.forEach(button => {
+    button.addEventListener("click", (e) => {
+
+        let value = e.target.innerText;
+
+        if (value === "AC") {
+            string = "";
+            input.value = string;
+        }
+        else if (value === "DEL") {
+            string = string.slice(0, -1);
+            input.value = string;
+        }
+        else if (value === "=") {
+            try {
+                string = eval(string);
+                input.value = string;
+            } catch {
+                input.value = "Error";
+            }
+        }
+        else {
+            string += value;
+            input.value = string;
+        }
+
+    });
+});
