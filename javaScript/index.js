31e20fa9e94558ca69ea2264ff4acfc9d50db3f5
jaya khandelwal <jayakhandelwal750@gmail.com>
2026-03-17 19:13:30 +0530

    first commit

---
+++ javaScript/index.js
@@ -0,0 +1,63 @@
+
+/*
+let a=10;
+let b=20;
+let c=a+b;
+console.log(c);
+
+
+
+Area of circle
+const pi=3.14;
+let r=parseInt(prompt("enter radius"))
+let r2=parseInt(prompt("enter radius"))
+let area=pi*r*r2;
+document.writeln(area)
+
+
+
+cube of a number
+let a=parseInt(prompt("enter the number"));
+let c=a*a*a;
+document.writeln(c);
+
+
+
+let a=parseInt(prompt("enter number 1"));
+let b=parseInt(prompt("enter number 2"));
+let c=a+b;
+console.log(c*c);
+
+
+
+let a=parseInt(prompt("enter number "));
+if (a%2==0){
+    console.log("number is even")
+}
+else{
+    console.log("odd")
+}
+
+
+let a=parseInt(prompt("enter number 1"));
+let b=parseInt(prompt("enter number 2"));
+if (a>b){
+alert("a is graeter")
+}
+if (a<b){
+alert("b is graeter")
+}*/
+
+
+let a=parseInt(prompt("enter number 1"));
+if(a>0){
+    alert(" a is postibe")
+}
+else if(a==0){
+    alert("it is neuratal")
+}
+else{
+    alert("it is negative")
+}
+
+
