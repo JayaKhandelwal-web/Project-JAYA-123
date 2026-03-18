f0a938d6969713fd910d40cc931f9e511a40e36d
jaya khandelwal <jayakhandelwal750@gmail.com>
2026-03-18 18:31:02 +0530

    first commit

---
+++ javaScript/first.js
@@ -0,0 +1,19 @@
+/*
+let ob={
+    name:"Jaya",
+    Age:22,
+    contact:123456
+}
+console.log(ob.Age);*/
+
+
+
+let list=document.getElementsByTagName('li')
+console.log(list);
+list[1].textContent="about"
+
+
+
+
+
+
